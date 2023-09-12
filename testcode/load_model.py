148900d29003f62f969fbc22a72ce4fe4da115ca
20171113 <syparksy98@unist.ac.kr>
2023-09-12 15:55:51 +0900

    feat: Add option for selecting RGB or gray

--- testcode/load_model.py
+++ testcode/load_model.py
@@ -1,11 +1,23 @@
 import torch
 import cv2
 import matplotlib.pyplot as plt
+import argparse
 
-v_path = '/Data3/soyeon/2023_ATC/anomal/code/yolov5/runs/train/exp13/weights/best.pt'
+parser = argparse.ArgumentParser()
+parser.add_argument('--color', type=str, default='color', help='gray or color')
+opt = parser.parse_known_args()[0]
+
+if opt.color == 'color':
+    v_path = '/Data3/soyeon/2023_ATC/anomal/code/yolov5/runs/train/exp13/weights/best.pt'
+    cvtcolor = cv2.COLOR_BGR2RGB
+
+else:
+    v_path = '/Data3/soyeon/2023_ATC/yolov5/runs/train/exp12/weights/best.pt'
+    cvtcolor = cv2.COLOR_BGR2GRAY
+        
 obj_path = '/Data3/soyeon/2023_ATC/yolov5/runs/train/exp2/weights/best.pt'
 
-cap = cv2.VideoCapture('/Data3/soyeon/2023_ATC/anomal/data/etc/CH003/[CH003] 20230707010206-20230707010408.avi')
+cap = cv2.VideoCapture('/Data3/soyeon/2023_ATC/anomal/data/etc/CH001/[CH001] 20230707021030-20230707021159.avi')
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=v_path) 
 
@@ -19,7 +31,8 @@ while True:
     
     if idx%5 != 0:
         continue
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    frame = cv2.cvtColor(frame, cvtcolor)
     frame = cv2.resize(frame, (int(3840/5), int(2160/5)))
     results = model(frame)
     results.save(save_dir='runs/detect/exp2/', exist_ok=True)    
